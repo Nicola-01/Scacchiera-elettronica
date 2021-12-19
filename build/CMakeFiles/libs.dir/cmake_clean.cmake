f047c1b8fa52eadf27b6727c8aa1292770be2fa1
Nicola <nickbusato0101@gmail.com>
2021-12-19 21:29:48 +0100

    Cmake

---
+++ build/CMakeFiles/libs.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libs.dir/src/Chessboard.cpp.o"
+  "CMakeFiles/libs.dir/src/Chessboard.cpp.o.d"
+  "CMakeFiles/libs.dir/src/Piece.cpp.o"
+  "CMakeFiles/libs.dir/src/Piece.cpp.o.d"
+  "liblibs.a"
+  "liblibs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
