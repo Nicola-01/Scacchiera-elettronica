a40308ae5ca8098cf402f81e2c13b5f2fe2b86b6
rickm01 <riccardo.miele@studenti.unipd.it>
2021-12-22 11:43:01 +0100

    Piece.h

--- src/Piece.cpp
+++ src/Piece.cpp
@@ -4,7 +4,11 @@
 #define Piece_hpp
 
 #include "Piece.h" 
-
+Piece::Piece(){
+    white = false;
+    ex_position_x = -1;
+    ex_position_y = -1;
+}
 //Pezzo (variabili colore, tipo) (funzioni move, isValidMove(?), cout)
 Re::Re(bool color, int y, int x) : Piece(color, y, x)
     {
@@ -106,48 +110,7 @@ Pedone::Pedone(bool color, int y, int x) : Piece(color, y, x)
     template <int Y, int X>
 bool Cavallo::is_valid_move(Piece (&Board)[Y][X], int str_x, int str_y, int end_x, int end_y)
 {
-    int delta_x = abs(str_x - end_x);
-    int delta_y = abs(str_y - end_y);
-    if (is_white == Board[end_y][end_x].is_white)   //posso crearlo nella classe padre is_friend
-        return false; //destinazione diverso colore;
-    return (delta_x <= 2 && delta_y <= 1) || (delta_x <= 1 && delta_y <= 2);
 
-
+   // return (abs(delta_x) <= 2 && abs(delta_y) <= 1) || (abs(delta_x) <= 1 && abs(delta_y) <= 2)
 };
-
-template <int Y, int X>
-bool Torre::is_valid_move(Piece (&Board)[Y][X], int str_x, int str_y, int end_x, int end_y) //meglio nel privato
-{
-    int delta_x = abs(str_x - end_x);
-    int delta_y = abs(str_y - end_y);
-    if (is_white == Board[end_y][end_x].is_white)   //posso crearlo nella classe padre is_friend
-        return false; //destinazione diverso colore;
-    if (abs(delta_x) != 0 && abs(delta_y) != 0)
-        return false;
-
-    for (int i=0; i<delta_x; i++)
-    {
-        if (end_x > str_x)
-            if (Board[end_y][str_x + i].print() != ' ')
-                return false;
-        else
-            if (Board[end_y][str_x - i].print() != ' ')
-                return false;
-    }
-    for (int i=0; i<delta_y; i++)
-    {
-        if (end_y > str_y)
-            if (Board[str_y + i][end_y].print() != ' ')
-                return false;
-        else
-            if (Board[str_y - i][end_y].print() != ' ')
-                return false;
-    }
-    return true;
-};
-
-
-
-
-
 #endif
