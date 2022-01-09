0fae7c52dee138b78a55ba80436b6afd4028a131
Nicola-01 <96294696+Nicola-01@users.noreply.github.com>
2022-01-09 19:39:22 +0100

    Add files via upload

--- include/Chessboard.h
+++ include/Chessboard.h
@@ -20,7 +20,7 @@ class Chessboard
 {
 private:
     //--- variables
-    Piece board[8][8]; //[y][x]
+    Piece *board[8][8]; //[y][x]
     const std::string pos{"TCADRACT"}; // è la "sequenza" in cui vengono posizionati i pezzi diversi dal pedone
     //const std::string pos{"T  DR  T"}; // easy arrocco
     int king_white[2]{7, 4}; //y x
@@ -30,7 +30,7 @@ private:
     int last_capture;
 
     //--- methods
-    Piece inizializer_piece(char p, int y, int x);
+    Piece *inizializer_piece(char p, int y, int x);
     bool is_valid_string(std::string move);
 
     std::pair<int, int> direction_threat(int king_y, int king_x, bool black_king, int dir_y, int dir_x);
@@ -43,18 +43,18 @@ public:
     Chessboard();
     int move(std::string& s_move, bool white_turne) { return move(s_move, white_turne, false); }
     int move(std::string& s_move, bool white_turne, bool replay);
-    bool is_right_piece(int y, int x, bool white_turne) { return (board[y][x].print() != ' ' && board[y][x].is_white() == white_turne); };
+    bool is_right_piece(int y, int x, bool white_turne) { return (board[y][x]->print() != ' ' && board[y][x]->is_white() == white_turne); };
     std::string random_move(int y, int x, bool white_turne);
-    char pices_type(int y, int x) { return board[y][x].print(); }
+    char pices_type(int y, int x) { return board[y][x]->print(); }
 
 
     //bool is_checkmate(bool in_black, int st_y, int st_x, int end_y, int end_x) { return false; };
     //int is_check(bool in_black, int st_y, int st_x, int end_y, int end_x) { return false; };
     //bool is_draw(int end_y, int end_x) { return false; };
     
-    bool is_checkmate(bool in_black, int st_y, int st_x, int end_y, int end_x);
-    int is_check(bool in_black, int st_y, int st_x, int end_y, int end_x);
-    bool is_draw(int end_y, int end_x);
+    bool is_checkmate(bool in_black, int st_y, int st_x, int end_y, int end_x){return false;};
+    int is_check(bool in_black, int st_y, int st_x, int end_y, int end_x){return false;};
+    bool is_draw(int end_y, int end_x) {return false;};
     
     bool is_checkmate(bool in_black) { return is_checkmate(in_black, str[0], str[1], end[0], end[1]); }
     int is_check(bool in_black) { return is_check(in_black, str[0], str[1], end[0], end[1]); }
